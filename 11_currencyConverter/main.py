778c08a691be44e05dc9f383dd382418ced8baa5
Wolgang <kralicek.roman@gmail.com>
2024-01-14 10:44:24 +0100

    working on converter

--- 11_currencyConverter/main.py
+++ 11_currencyConverter/main.py
@@ -1,24 +1,64 @@
-import tkinter
+import tkinter as tk
+import tkinter.ttk as ttk
 
-window = tkinter.Tk()
+class LayoutManager:
+    def __init__(self) -> None:
+        self.rows = 0
+        self.cols = 0
+    def newRow(self):
+        self.rows += 1
+        return self.rows
+    def newCol(self):
+        self.cols += 1
+        return self.cols
+    def currCol(self):
+        return self.cols
+    def currRow(self):
+        return self.rows
 
-win_width = 500
-win_height = 500
+layout = LayoutManager()
 
-while True:
-    window.title("test")
-    window.minsize(width=win_width, height=win_height)
+# creating main tkinter window/toplevel
+main_win = tk.Tk()
+main_win.title("Currency converter")
 
-    my_button = tkinter.Button(text="button")
-    my_button.pack()
 
-    my_frame = tkinter.Frame()
-    my_frame.pack()
+# this will create a label widget
+l1 = ttk.Label(main_win, text = "First:")
+l2 = ttk.Label(main_win, text = "Second:")
+l3 = ttk.Label(main_win, text = "Third:")
+l4 = ttk.Label(main_win, text = "Fourth:")
 
-    my_label = tkinter.Label(text="label", font=("Arial", 24, "bold"))
-    my_label.pack()
+# entry widgets, used to take entry from user
+e1 = ttk.Entry(main_win)
+e2 = ttk.Entry(main_win)
 
-    window.mainloop()
+def rename():
+    global l1
+    l1["text"] = "changed label asdf asdfasdf asdkjfhas;dkfh as;dkfhj ask;fhj;asdkfh j;asdkfgh jsa asdfas dfasd fasdf asg dfghdsh d"
+
+btn1 = ttk.Button(text = "change", command=rename)
+
+# grid method to arrange labels in respective
+# rows and columns as specified
+l1.grid(row = layout.currRow(), column = layout.currCol(), columnspan=30, sticky = tk.W, pady = 2)
+l2.grid(row = layout.newRow(), column = layout.newCol(), sticky = tk.W, pady = 2)
+l3.grid(row = layout.newRow(), column = layout.newCol(), sticky = tk.W, pady = 2)
+l4.grid(row = layout.newRow(), column = layout.newCol(), sticky = tk.W, pady = 2)
+e1.grid(row = layout.newRow(), column = layout.newCol(), sticky = tk.W, pady = 2)
+e2.grid(row = layout.newRow(), column = layout.newCol(), sticky = tk.W, pady = 2)
+btn1.grid(row = layout.newRow(), column = layout.newCol(), sticky = tk.W, pady = 2)
+
+
+
+
+# # this will arrange entry widgets
+# e1.grid(row = 0, column = 1, pady = 2)
+# e2.grid(row = 1, column = 1, pady = 2)
+ 
+# # infinite loop which can be terminated by keyboard
+# # or mouse interrupt
+tk.mainloop()
 
 #TODO make currency converter which connects on the internet and gather data through some API
     # EURO, CZK, USD, GBP, KRW
--- test.py
+++ test.py
@@ -1,24 +1,3 @@
-import random 
+import win32com.client
 
-
-rrr = 0.5 #risk/reward ratio
-
-risk_perc = 0.01
-profit_perc = risk_perc * rrr
-
-starting_account = 100
-
-num_trades = 100
-num_traders = 10
-
-trader_result = []
-
-for i in range(num_traders):
-    for j in range(num_trades):
-
-
-for result in trader_result:
-    pass
-
-
-
+app = win32com.client.Dispatch("Autocad.AdvanceSteel")
