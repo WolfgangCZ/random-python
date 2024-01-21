6881b5d26fc94472c7d9aa7364d0052779469ec5
Wolgang <kralicek.roman@gmail.com>
2024-01-21 21:20:51 +0100

    aaa

--- 14_vim/main.py
+++ 14_vim/main.py
@@ -1,11 +1,27 @@
 print("hello world")
 
+
 def func(x):
     print(f"inside {x}")
+
 
 class test:
     def __init__(self):
         pass
 
+
 test_class = test()
 func("hello from the funciton")
+
+
+class pepa:
+    def __init():
+        pass
+
+
+mason = 1
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for item in lst:
+    print(item)
