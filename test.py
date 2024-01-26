cd239eae449684637dd1dcfe023e1cdb0d73b110
Wolgang <kralicek.roman@gmail.com>
2024-01-26 17:22:27 +0100

    testing

--- test.py
+++ test.py
@@ -4,3 +4,25 @@ dirname = os.path.dirname(__file__)
 sys.path.append(dirname)
 
 print(f"dir: {dirname}")
+
+class TestClass:
+    def __init__(self):
+        pass
+    
+    def do1(self):
+        print("im doing 1")
+    def do2(self):
+        print("im doing 2")
+
+t = TestClass()
+
+
+
+a = {"a": t.do1(),
+     "b": t.do2()}
+def do(name, data):
+   return data[name] 
+    
+
+do("a", a)
+do("b", a)
